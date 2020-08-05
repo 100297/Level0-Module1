9c5bd90d5fce31d9dbb918cf56b4ff87883be0f3
100297 <993421Zach>
2020-08-05 21:56:06 +0000

    Day_3

--- _02_variables_practice/circle_area_calculator.py
+++ _02_variables_practice/circle_area_calculator.py
@@ -12,24 +12,25 @@ if __name__ == '__main__':
     window.withdraw()
     
     # Ask the user for the radius in pixels and store it in a variable
-    # simpledialog.askinteger()
+    ans = simpledialog.askinteger(title = "question", prompt = "what shall the radius be?")
     
     # Make a new turtle
-    
+    Zach = turtle.Turtle()
     # Have your turtle draw a circle with the correct radius
     # my_turtle.circle()
-
+    Zach.circle(ans)
     # Call the turtle .penup() method
-
+    Zach.penup()
     # Move your turtle to a new x,y position using .goto()
-
+    Zach.goto(30,30)
     # Calculate the area of your circle and store it in a variable, you can use math.pi
-    
+    area = math.pi*ans*ans/4
     # Write the area of your circle using the turtle .write() method
     # myTurtle.write(arg="area = " + str(area), move=True, align='left', font=('Arial',8,'normal'))
-
+    Zach.write(arg="area = " + str(area), move=True, align='left', font=('Arial',8,'normal'))
     # Hide your turtle
-
+    Zach.hideturtle()
     # Call turtle.done()
+    #Zach.done()
 
     window.mainloop()
--- _03_if_else/_4_tasty_tomato/tasty_tomato.py
+++ _03_if_else/_4_tasty_tomato/tasty_tomato.py
@@ -1,4 +1,4 @@
-from tkinter import simpledialog, messageobx, Tk, Canvas
+from tkinter import simpledialog, messagebox, Tk, Canvas
 
 windowWidth = 600
 windowHeight = 600
@@ -10,12 +10,12 @@ canvas.grid()
 
 #1. Ask the user what color tomato they would like and save their response   
 #   You can give them up to three choices 
-
+color = simpledialog.askstring(title="question", prompt="what color do you want the tomato")
 
 #2. use if-else statements to draw the tomato in the color that they chose
 #   you can modify the code below or draw your own tomato
-canvas.create_oval(75, 200, 400, 450, fill="red", outline="")
-canvas.create_oval(200, 200, 525, 450, fill="red", outline="")
+canvas.create_oval(75, 200, 400, 450, fill=color, outline="")
+
 
 
 canvas.create_rectangle(275, 100, 325, 230, fill="green", outline="")
